82fd0b3a6beb340ac7009b331965c4cb3d9e8bc3
ileniapinna <55718312+ileniapinna@users.noreply.github.com>
2023-02-17 14:30:40 +0100

    Update MainServer.java

--- MainServer/src/mainserver/MainServer.java
+++ MainServer/src/mainserver/MainServer.java
@@ -6,20 +6,20 @@ package mainserver;
 
 /**
  *
- * @author vicbo
+ * @author Ilenia Pinna
  */
+
+//Questa classe serve per attivare i thread della classe server
 public class MainServer {
-     /**
-     * Metodo statico per l'avvio della classe.
-     * @param args argomenti da linea di comando
-     */
+   
+//Metodo statico per l'avvio della classe. Il server sta sulla porta 2000
     public static void main(String[] args) {
-        Server s = new Server(2000);
+        Server s = new Server(2000);  
         if (s != null) {
-                s.attendi();
-                s.leggi();
-                s.scrivi();
-                s.chiudi();
+                s.attendi();   //attende che il client gli invia un messaggio
+                s.leggi();     //legge il messaggio
+                s.scrivi();    //scrive il messaggio
+                s.chiudi();    //chiude la connessione
             }
     }
 }
